14683c5440ff5f7e899223e59c677ab03a35c205
valdinei-ads <valdinei.ads@gmail.com>
2020-05-01 20:20:13 -0300

    Initial Commit

---
+++ CursoDesignPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
